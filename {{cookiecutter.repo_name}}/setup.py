ab1301351f1325693731d46067061ee8ceea4161
michael sedelmeyer <20605812+sedelmeyer@users.noreply.github.com>
2019-04-29 15:17:46 -0400

    fix(setup): change python_requires to >=3.5 and add scm_versioning logic

--- {{cookiecutter.repo_name}}/setup.py
+++ {{cookiecutter.repo_name}}/setup.py
@@ -143,7 +143,7 @@ setup(
     keywords=[
         # eg: 'keyword1', 'keyword2', 'keyword3',
     ],
-    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
+    python_requires='>=3.5',
     install_requires=[
 {%- if cookiecutter.command_line_interface == 'click' %}
         'click',
@@ -191,6 +191,10 @@ setup(
 {%- if cookiecutter.c_extension_support == 'cython' %} '*.pyx' if Cython else '*.c'
 {%- else %} '*.c'{% endif %}))
     ],
+{%- if cookiecutter.scm_versioning == 'yes' %}
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
+{%- endif %}
 {%- endif %}
 {%- endif %}
 )
